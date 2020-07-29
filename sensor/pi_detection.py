4244d76a128ce6db57568a8d504108a185ded8c3
JuliaDi <di.julia.8@gmail.com>
2020-07-29 09:24:09 -0700

    motion detection

---
+++ sensor/pi_detection.py
@@ -0,0 +1,28 @@
+# import the necessary packages
+from pyimagesearch.tempimage import TempImage
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import argparse
+import warnings
+import datetime
+import dropbox
+import imutils
+import json
+import time
+import cv2
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-c", "--conf", required=True,
+	help="path to the JSON configuration file")
+args = vars(ap.parse_args())
+# filter warnings, load the configuration and initialize the Dropbox
+# client
+warnings.filterwarnings("ignore")
+conf = json.load(open(args["conf"]))
+client = None
+
+# check to see if the Dropbox should be used
+if conf["use_dropbox"]:
+	# connect to dropbox and start the session authorization process
+	client = dropbox.Dropbox(conf["dropbox_access_token"])
+	print("[SUCCESS] dropbox account linked")
